e8c61f3c3eff425a8b65b65d0c73ff863ba5e61f
LetsWorkb1 <jhow.win@gmail.com>
2022-07-11 19:15:29 -0300

    Criação do conteúdo da primeira aula da nova trilha do ignite

---
+++ src/components/Comment.jsx
@@ -0,0 +1,44 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment () {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/jhowwn.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Jhonatan</strong>
+                            <time title='11 de Maio as 08:05h' dateTime='2022-05-11 08:05:30'>Cerca de 1h atrás</time>
+                        </div>
+                        <button onClick={handleDeleteComment} title="Deletar" >
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir<span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
